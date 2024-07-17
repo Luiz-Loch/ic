322f5ab1cc863c1b0900f32d64ab04b36d312c02
Luiz Loch <118476368+Luiz-Loch@users.noreply.github.com>
2024-07-16 21:33:18 -0300

    Adição do repositório de testes.

---
+++ tests/AWS.py
@@ -0,0 +1,22 @@
+import modulos
+
+if __name__ == '__main__':
+    BUCKET = 'benchmarks-ic'
+    TESTE_FILE = 'teste.txt'
+    instance_id = modulos.get_instance_id()
+    instance_name = modulos.get_instance_name(instance_id)
+
+    print('Iniciando o script...')
+
+    print(f'ID da Instância: {instance_id}')
+    if instance_name:
+        print(f'Nome da Instância: {instance_name}')
+    else:
+        print('Nome da Instância não encontrado')
+
+    # O arquivo `execution_times.json` é enviado para o bucket com o nome `execution_times.json`
+    s3_file = f'{instance_name}/Teste'
+    if modulos.upload_to_aws(TESTE_FILE, BUCKET, s3_file):
+        print(f'Arquivo `execution_times.json` enviado para o bucket {BUCKET} com sucesso!')
+    else:
+        print(f'Erro ao enviar o arquivo `execution_times.json` para o bucket {BUCKET}')
